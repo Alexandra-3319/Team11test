93fe850a394cdb9f6c822eca4b21bd5491273f8c
Alexandra-3319 <kran0012@hz.nl>
2023-01-22 17:50:27 +0100

    deleted button and readded

--- build/app.js
+++
@@ -1,8 +0,0 @@
-import CoinEarner from './CoinEarner.js';
-import { GameLoop } from './GameLoop.js';
-const game = new CoinEarner(document.getElementById('game'));
-const gameLoop = new GameLoop(game);
-window.addEventListener('load', () => {
-    gameLoop.start();
-});
-//# sourceMappingURL=app.js.map
